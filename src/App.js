e37966815107da35f189bae54726ce3199316395
rubaba.nagiyeva@mail.ru <rubaba.nagiyeva@mail.ru>
2023-08-20 08:59:14 -0700

    searchSortReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+const [person,setPerson] = useState({
+  name:"",
+  price:"",
+  image: ""
+})
+const [item,setItem] = useState([]);
+const [search,setSearch] = useState("");
+function handleInp(e){
+  setPerson({...person, [e.target.name]:e.target.value});
+}
+function saveData(e){
+e.preventDefault();
+setItem([...item,person]);
 }
 
-export default App;
+function sortData(){
+  setItem([...item.sort((a,b)=>a.price - b.price)])
+}
+
+function searchData(e){
+setSearch(e.target.value.toLowerCase());
+}
+
+return(
+<div>
+  <input name="name" onChange={handleInp} type="text" placeholder='Name'/> <br/> <br/>
+  <input name="price" onChange={handleInp} type="text" placeholder='Price'/> <br/> <br/>
+  <input name="image" onChange={handleInp} type="text" placeholder='Image'/> <br/> <br/>
+  <button className="btn" onClick={saveData} >Add</button> <br/> <br/>
+  <button className="btn" onClick={sortData}>Sort</button> <br/> <br/>
+  <input onChange={searchData} type="text" placeholder='Search'/>
+  <div className='dd'>
+  {
+    
+   item.filter(a=>a.name.toLowerCase().includes(search)).map((a,b)=>(
+    
+    <div className="d1" key={b}>
+      <h1>Name: {a.name}</h1>
+    <h2>Price: {a.price}</h2>
+    <img className='img1' src={a.image} alt=""/> 
+    </div>
+   
+    
+   ))
+  }
+</div>   
+</div>
+); 
+
+
+
+}
+export default App;  
